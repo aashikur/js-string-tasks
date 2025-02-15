bc42593395064c353404547e8a28cdc47e89eea9
aashikur <mdaashikur@gmail.com>
2025-02-15 07:24:39 +0600

    capital first word

--- Task-5.js
+++ Task-5.js
@@ -2,12 +2,16 @@
 // Capitalize Every first Letter of each word in a String
 // "# js-string-tasks" 
 
-const str = "A quick brown hungry fox jump over a Education Bar";
+
+// Capitalize Every First Letter
+const str = "a quick brown hungry fox jump over a Education Bar";
 
 
 // Spliting All -> single character array
 let arr = str.split('');
 for(let i = 0; i<arr.length; i++){
+
+    if(i == 0) arr[0] = arr[0].toUpperCase();
     if(arr[i] == ' '){
         arr[i+1] = arr[i+1].toUpperCase();
     }
